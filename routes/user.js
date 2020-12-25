29ac6188dbb95f02315f37767dcdbfe5ff8b726f
= <za.qa@outlook.com>
2020-12-25 09:22:10 +0000

    get users added

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-const {
-  createUser,
-  getAllUsers,
-  deleteUser,
-  patchUsers,
-} = require("../models/items");
+const { createUser, getAllUsers, patchUsers } = require("../models/items");
 
 /*---------Create User---------*/
 router.post("/", async function (req, res) {
@@ -15,18 +10,19 @@ router.post("/", async function (req, res) {
   res.json(user);
 });
 
-// /*---------GET: Get all users---------*/
-// router.get("/", function (req, res, next) {
-//   res.send("respond with a resource");
-// });
-
-/*---------DELETE: User---------*/
-router.delete("/:id", async function (req, res) {
-  let id = req.params.id;
-  console.log("delete id, routes: ", id);
-  deleteUser(id);
-  return res.json({ success: true });
+/*---------GET: Get all users---------*/
+router.get("/", async function (req, res) {
+  let users = await getAllUsers();
+  res.json({ success: true, payload: users });
 });
+
+// /*---------DELETE: User---------*/
+// router.delete("/:id", async function (req, res) {
+//   let id = req.params.id;
+//   console.log("delete id, routes: ", id);
+//   deleteUser(id);
+//   return res.json({ success: true });
+// });
 
 /*---------PATCH: User---------*/
 router.patch("/:id", async function (req, res) {
@@ -39,3 +35,8 @@ router.patch("/:id", async function (req, res) {
 });
 
 module.exports = router;
+
+// /*---------GET: Get all users---------*/
+// router.get("/", function (req, res, next) {
+//   res.send("respond with a resource");
+// });
