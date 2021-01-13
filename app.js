46c7432544907b30c7bdf63173794b5cf9d80746
= <za.qa@outlook.com>
2021-01-13 14:07:54 +0000

    limit lifted

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ var eventsRouter = require("./routes/events");
 var userEventsRouter = require("./routes/userEvents");
 var userJourneysRouter = require("./routes/userJourneys");
 
+// var bodyParser = require("body-parser");
+
 var app = express();
 
 //cors error
 app.use(cors());
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
