8941b1c3e3d1434b4dbfef063741b885995eebec
= <za.qa@outlook.com>
2021-01-15 13:43:04 +0000

    dev

--- models/nodemailer.js
+++ models/nodemailer.js
@@ -1,9 +1,6 @@
 const nodemailer = require("nodemailer");
 
 function sendEmail(userEmail, subject, text) {
-  console.log("this is email: ", userEmail);
-  console.log("this is subject: ", userEmail);
-  console.log("this is text: ", text);
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
     service: "hotmail",
@@ -29,13 +26,6 @@ function sendEmail(userEmail, subject, text) {
       console.log("Email sent: " + info.response);
     }
   });
-
-  //   console.log("Message sent: %s", info.messageId);
-  //   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  //   // Preview only available when sending through an Ethereal account
-  //   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  //   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 module.exports = { sendEmail };
